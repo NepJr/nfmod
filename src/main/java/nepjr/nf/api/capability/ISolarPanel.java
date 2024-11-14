46650279f85fa569d8c3f7293b7f7de09beb9814
NepJr <gudak0@protonmail.com>
2024-11-13 17:30:45 -0800

    updated ISolarPanel to be a little more descriptive

--- src/main/java/nepjr/nf/api/capability/ISolarPanel.java
+++ src/main/java/nepjr/nf/api/capability/ISolarPanel.java
@@ -7,5 +7,10 @@ public interface ISolarPanel
     * @return whether this solar panel can see the sun or not
     */
 	boolean canSeeSun();
+	
+	/**
+    *
+    * @return the tier of the solar panel as an integer (to use with GTValues.V[] or something)
+    */
 	int tier();
 }
