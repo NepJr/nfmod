4cde9799b6c57612d0283177cfe59c531dc2cab9
NepJr <gudak0@protonmail.com>
2024-11-18 15:45:16 -0800

    Large Centrifuging Machine initial commit

--- src/main/java/nepjr/nf/common/metatileentities/NFMetaTileEntities.java
+++ src/main/java/nepjr/nf/common/metatileentities/NFMetaTileEntities.java
@@ -6,19 +6,27 @@ import static nepjr.nf.NepFactory.nfId;
 import gregtech.api.GTValues;
 import nepjr.nf.common.metatileentities.electric.MetaTileEntityWeatherRepeller;
 import nepjr.nf.common.metatileentities.multiblock.generator.MetaTileEntitySolarArray;
+import nepjr.nf.common.metatileentities.multiblock.normal.MetaTileEntityLargeCentrifugingMachine;
 import nepjr.nf.common.metatileentities.multiblock.part.MetaTileEntitySolarPanelPart;
 
 public class NFMetaTileEntities
 {
+	// Multiblock Generators
 	public static MetaTileEntitySolarArray[] SOLAR_ARRAY = new MetaTileEntitySolarArray[GTValues.V.length];
 	
+	// Multiblock Machines
+	public static MetaTileEntityLargeCentrifugingMachine LARGE_CENTRIFUGE;
+	// Multiblock Parts
 	public static MetaTileEntitySolarPanelPart[] SOLAR_PANEL_PART = new MetaTileEntitySolarPanelPart[GTValues.V.length];
 	
+	// Special Singleblock Machines
 	public static MetaTileEntityWeatherRepeller WEATHER_REPELLER;
 	
 	public static void init()
 	{
 		WEATHER_REPELLER = registerMetaTileEntity(4000, new MetaTileEntityWeatherRepeller(nfId("weather_repeller")));
+		
+		LARGE_CENTRIFUGE = registerMetaTileEntity(4001, new MetaTileEntityLargeCentrifugingMachine(nfId("large_centrifuge")));
 		
         for (int i = 1; i < GTValues.V.length; i++) {
         	SOLAR_PANEL_PART[i] = new MetaTileEntitySolarPanelPart(nfId("solar_panel_part." + GTValues.VN[i].toLowerCase()), i);
--- src/main/java/nepjr/nf/common/metatileentities/multiblock/normal/MetaTileEntityLargeCentrifugingMachine.java
+++ src/main/java/nepjr/nf/common/metatileentities/multiblock/normal/MetaTileEntityLargeCentrifugingMachine.java
@@ -6,14 +6,20 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
 import nepjr.nf.api.metatileentity.multiblock.NFMultiblockController;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class MetaTileEntityLargeCentrifugingMachine extends NFMultiblockController
 {
-
 	public MetaTileEntityLargeCentrifugingMachine(ResourceLocation metaTileEntityId) {
 		super(metaTileEntityId, RecipeMaps.CENTRIFUGE_RECIPES);
 	}
@@ -21,14 +27,32 @@ public class MetaTileEntityLargeCentrifugingMachine extends NFMultiblockControll
 	@Override
 	protected @NotNull BlockPattern createStructurePattern() {
 		// TODO Auto-generated method stub
-		return null;
+		return FactoryBlockPattern.start()
+				.aisle("CCC", "CCC", "CCC")
+				.aisle("CCC", "CCC", "CCC")
+				.aisle("CCC", "CSC", "CCC")
+				.where('s', selfPredicate())
+				.where('c', states(getCasingState())
+						.or(autoAbilities()))
+				.build();
 	}
 
 	@Override
 	public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
 		// TODO Auto-generated method stub
-		return null;
+		return Textures.ROBUST_TUNGSTENSTEEL_CASING;
 	}
+	
+    protected IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.TUNGSTENSTEEL_ROBUST);
+    }
+	
+	@SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.LARGE_STEAM_TURBINE_OVERLAY;
+    }
 
 	@Override
 	public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
