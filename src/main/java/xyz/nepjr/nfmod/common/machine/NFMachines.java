3a3182cbbce3ca46f457b4d5816852d53bbc431a
NepJr <gudak0@protonmail.com>
2026-01-29 13:57:49 -0800

    fixed a formatting issue, added new recipe map, implemented setting username and uuid

--- src/main/java/xyz/nepjr/nfmod/common/machine/NFMachines.java
+++ src/main/java/xyz/nepjr/nfmod/common/machine/NFMachines.java
@@ -25,37 +25,38 @@ import xyz.nepjr.nfmod.common.recipes.NFRecipeTypes;
 public class NFMachines 
 {
 	public static final MachineDefinition[] MATTER_FABRICATOR = registerSimpleMachines("matter_fabricator", NFRecipeTypes.UU_MATTER_FABRICATOR, defaultTankSizeFunction, false, "UU-Matter Fabricator", GTValues.tiersBetween(EV, UHV));
+	public static final MachineDefinition[] HIGH_TEMP_STEAM_TURBINE = registerSimpleMachines("high_temp_steam_turbine", NFRecipeTypes.HIGH_TEMP_STEAM_TURBINE, defaultTankSizeFunction, false, "High-Temperature Steam Turbine", GTValues.ALL_TIERS);
 	
 	public static void init() {}
 	
-	// Borrowed from GTCA, no idea why this shit is fucked up though
-    public static MachineDefinition[] registerSimpleMachines(String name,
-            GTRecipeType recipeType,
-            Int2IntFunction tankScalingFunction,
-            boolean hasPollutionDebuff,
-            String lang,
-            int... tiers) {
-return registerTieredMachines(name,
-(holder, tier) -> new SimpleTieredMachine(holder, tier, tankScalingFunction), (tier, builder) -> {
-if (hasPollutionDebuff) {
-builder.recipeModifiers(GTRecipeModifiers.ENVIRONMENT_REQUIREMENT
-.apply(GTMedicalConditions.CARBON_MONOXIDE_POISONING, 100 * tier),
-GTRecipeModifiers.OC_NON_PERFECT)
-.conditionalTooltip(defaultEnvironmentRequirement(),
-ConfigHolder.INSTANCE.gameplay.environmentalHazards);
-} else {
-builder.recipeModifier(GTRecipeModifiers.OC_NON_PERFECT);
-}
-return builder
-.langValue("%s %s %s".formatted(VLVH[tier], lang, VLVT[tier]))
-.editableUI(SimpleTieredMachine.EDITABLE_UI_CREATOR.apply(NepFactory.id(name), recipeType))
-.rotationState(RotationState.NON_Y_AXIS)
-.recipeType(recipeType)
-.workableTieredHullModel(NepFactory.id("block/machines/" + name))
-.tooltips(workableTiered(tier, GTValues.V[tier], GTValues.V[tier] * 64, recipeType,
-tankScalingFunction.apply(tier), true))
-.register();
-},
-tiers);
-}
+	// Borrowed from GTCA
+	    public static MachineDefinition[] registerSimpleMachines(String name,
+                                                             GTRecipeType recipeType,
+                                                             Int2IntFunction tankScalingFunction,
+                                                             boolean hasPollutionDebuff,
+                                                             String lang,
+                                                             int... tiers) {
+        return registerTieredMachines(name,
+                (holder, tier) -> new SimpleTieredMachine(holder, tier, tankScalingFunction), (tier, builder) -> {
+                    if (hasPollutionDebuff) {
+                        builder.recipeModifiers(GTRecipeModifiers.ENVIRONMENT_REQUIREMENT
+                                                .apply(GTMedicalConditions.CARBON_MONOXIDE_POISONING, 100 * tier),
+                                        GTRecipeModifiers.OC_NON_PERFECT)
+                                .conditionalTooltip(defaultEnvironmentRequirement(),
+                                        ConfigHolder.INSTANCE.gameplay.environmentalHazards);
+                    } else {
+                        builder.recipeModifier(GTRecipeModifiers.OC_NON_PERFECT);
+                    }
+                    return builder
+                            .langValue("%s %s %s".formatted(VLVH[tier], lang, VLVT[tier]))
+                            .editableUI(SimpleTieredMachine.EDITABLE_UI_CREATOR.apply(NepFactory.id(name), recipeType))
+                            .rotationState(RotationState.NON_Y_AXIS)
+                            .recipeType(recipeType)
+                            .workableTieredHullModel(NepFactory.id("block/machines/" + name))
+                            .tooltips(workableTiered(tier, GTValues.V[tier], GTValues.V[tier] * 64, recipeType,
+                                    tankScalingFunction.apply(tier), true))
+                            .register();
+                },
+                tiers);
+    }
 }
--- src/main/java/xyz/nepjr/nfmod/common/recipes/NFRecipeTypes.java
+++ src/main/java/xyz/nepjr/nfmod/common/recipes/NFRecipeTypes.java
@@ -18,4 +18,11 @@ public class NFRecipeTypes
             .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, ProgressTexture.FillDirection.LEFT_TO_RIGHT)
             .setSound(GTSoundEntries.CHEMICAL)
             .setEUIO(IO.IN);
+	
+	public static final GTRecipeType HIGH_TEMP_STEAM_TURBINE = register("high_temp_steam_turbine", GENERATOR)
+			.setMaxIOSize(0, 0, 1, 0)
+			.setSlotOverlay(true, true, GuiTextures.FLUID_SLOT)
+			.setProgressBar(GuiTextures.PROGRESS_BAR_GAS_COLLECTOR, ProgressTexture.FillDirection.DOWN_TO_UP)
+			.setSound(GTSoundEntries.TURBINE)
+			.setEUIO(IO.OUT);
 }
